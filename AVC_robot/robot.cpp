f65c96b487ce8031b966bcd88765998841566fd2
isaacy2012 <50868057+isaacy2012@users.noreply.github.com>
2020-06-13 00:40:30 +1200

    Line optimisation
    Did away with vectors and now just use a new|old integer pair to store the differences. Using this and some other optimisations, managed to get the code down to 15 lines/semicolons. Could should be functionally the same as before. Awaiting testing.

--- AVC_robot/robot.cpp
+++ AVC_robot/robot.cpp
@@ -1,11 +1,10 @@
 #include "robot.hpp"
 #include <cmath>
-using namespace std;
 
-int WHITE_THRESH = 254, RED_RATIO = 3, motorSpeed = 40, maxLines = 10, safety = 68, defaultLeft = 12, redMode = 0;
+int WHITE_THRESH = 254, RED_RATIO = 3, motorSpeed = 40, maxLines = 10, safety = 68, defaultLeft = 12, redMode = 0, previousDistToFront = -1, avgX = -1, xTotal = 0, counter = 0 ;
 
 int getMotorDifference ( int xError) {
-    //https://www.desmos.com/calculator/xsjgpwoe0c
+    //https://www.desmos.com/calculator/sze5nvrrbr
     return (int)(60*sin( (xError/300.0)* M_PI ));
 }
 
@@ -28,14 +27,10 @@ int main() {
     //camera initialisation
     cameraView.width = 150, cameraView.height = 100, cameraView.data = new char[cameraView.width * cameraView.height * 3], cameraView.n_bytes = cameraView.width * cameraView.height * 3;
     if (initClientRobot() != 0) {
-        cout << " Error initializing robot" << endl;
+        std::cout << " Error initializing robot" << std::endl;
     }
-    //vector to store the distToFrontValues so that we can check if it's decreasing or not
-    vector<int> distToFrontValues;
     while (true) {
-        takePicture();
-        int motorDifference = 0, distToFront = distanceToColor(90, 40), distToLeft = distanceToColor(180, (int)(cameraView.width/2.0)), distToRight = distanceToColor( 0,50);
-        distToFrontValues.push_back(distToFront);
+        int pictureState = takePicture(), motorDifference = 0, distToFront = distanceToColor(90, 40), distToLeft = distanceToColor(180, (int)(cameraView.width/2.0)), distToRight = distanceToColor( 0,50);
         if (distToLeft > -1 ) {
             //if there's something to the left, suggest that the bot moves right
             motorDifference = getMotorDifference(safety - distToLeft);
@@ -43,14 +38,13 @@ int main() {
             //if there's nothing to the left, bot go left by default slowly (full turn)
             motorDifference = getMotorDifference(-defaultLeft);
         }
-        if (distToFrontValues[distToFrontValues.size()-2] > distToFront && distToFront > -1) {
+        if (distToFront < previousDistToFront && distToFront > -1 ) {
             //impedance from the front
             motorDifference = motorDifference+(int)(90-distToFront), redMode = 1;
         } else {
             //nothing impeding from the front
         }
         //finding average x of white pixels
-        int avgX = -1, xTotal = 0, counter = 0;
         for (int i = 0; i < maxLines; i++ ) {
             for (int j = 0; j < cameraView.width; j ++ ) {
                 if ((int) get_pixel(cameraView, cameraView.height-i, j, 3) > WHITE_THRESH) {
@@ -61,16 +55,13 @@ int main() {
         //cout << "Left: " << distToLeft << " Right: " << distToRight << " Motor: " << motorDifference << endl
         if (counter > 10) {
             //if there is a significant number of white pixels, follow the white line
-            //avgX = xTotal/counter //, avgY = yTotal/counter
             //-1 is so that if it's equal on both sides, it will always go left
             avgX = xTotal/counter, motorDifference = getMotorDifference( avgX - cameraView.width/2 -1);
         } else if (motorDifference == getMotorDifference(-defaultLeft) && distToLeft == -1 && distToRight == -1 && redMode == 0 ) {
             //if redmode is off and there's no other stuff to guide the bot, spin around quickly since that's a dead end
             motorDifference = getMotorDifference(-cameraView.width);
         }
-        //set the motor velocities.
-        int vLeft = motorSpeed+motorDifference, vRight = motorSpeed-motorDifference;
-        setMotors(vLeft, vRight);
-        usleep(500);
+        //set the motors to the velocities, and reset variables, and usleep
+        setMotors(motorSpeed+motorDifference, motorSpeed-motorDifference), previousDistToFront = distToFront, avgX = -1, xTotal = 0, counter = 0, usleep(10000);
     } //while
 } // main
